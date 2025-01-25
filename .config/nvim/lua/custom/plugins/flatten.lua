fcd07573250211cc62193e457a563d24dbc09146
Olafur Karl Stefansson <olik@olik.dev>
2025-01-25 00:42:34 +0000

    Fix tests again

--- .config/nvim/lua/custom/plugins/flatten.lua
+++ .config/nvim/lua/custom/plugins/flatten.lua
@@ -1,20 +1,20 @@
 return {
-	{
-		-- Open files and command output from wezterm, kitty, and neovim terminals in
-		-- your current neovim instance
-		-- https://github.com/willothy/flatten.nvim
-		"willothy/flatten.nvim",
 
-		-- Ensure that it runs first to minimize delay when opening file from terminal
-		lazy = false,
-		priority = 1001,
-		enabled = true,
-		opts = function()
-			return {
-				window = {
-					open = "alternate",
-				},
-			}
-		end,
-	},
+	-- Open files and command output from wezterm, kitty, and neovim terminals in
+	-- your current neovim instance
+	-- https://github.com/willothy/flatten.nvim
+	"willothy/flatten.nvim",
+
+	-- Ensure that it runs first to minimize delay when opening file from terminal
+	lazy = false,
+	priority = 1001,
+	-- this can not be enabled until https://github.com/willothy/flatten.nvim/issues/106 is solved
+	enabled = false,
+	opts = function()
+		return {
+			window = {
+				open = "alternate",
+			},
+		}
+	end,
 }
