e32b9602eb3a014403786f46820a67af2bb98a50
Olafur Karl Stefansson <olik@olik.dev>
2025-02-21 17:45:31 +0000

    Snippets + kulalla + misc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -302,17 +302,9 @@ require("lazy").setup({
 		},
 		config = function()
 			local image_preview = require("custom.functions.telescope_image_preview").telescope_image_preview()
-			-- [[ Configure Telescope ]]
 			-- See `:help telescope` and `:help telescope.setup()`
+			local actions = require("telescope.actions")
 			require("telescope").setup({
-				-- You can put your default mappings / updates / etc. in here
-				--  All the info you're looking for is in `:help telescope.setup()`
-				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
 				pickers = {
 					find_files = {
 						hidden = true,
@@ -337,6 +329,9 @@ require("lazy").setup({
 					},
 				},
 				defaults = {
+					mappings = {
+						i = { ["<esc>"] = actions.close },
+					},
 					file_previewer = image_preview.file_previewer,
 					grep_previewer = image_preview.grep_previewer,
 					buffer_previewer_maker = image_preview.buffer_previewer_maker,
@@ -730,21 +725,14 @@ require("lazy").setup({
 					return "make install_jsregexp"
 				end)(),
 				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
 					{
 						"rafamadriz/friendly-snippets",
-						config = function()
-							require("luasnip.loaders.from_vscode").lazy_load()
-						end,
 					},
 					{
 						"solidjs-community/solid-snippets",
 					},
 				},
 				config = function()
-					require("luasnip.loaders.from_vscode").lazy_load()
 					local ls = require("luasnip")
 					ls.config.set_config({
 						region_check_events = "InsertEnter",
@@ -765,6 +753,18 @@ require("lazy").setup({
 					vim.keymap.set({ "i", "s" }, "<C-n>", function()
 						ls.jump(-1)
 					end, { silent = true })
+
+					local snippetPaths = vim.list_extend(
+						vim.api.nvim_get_runtime_file("package.json", true),
+						vim.api.nvim_get_runtime_file("snippets/package.json", true)
+					)
+					for i, str in ipairs(snippetPaths) do
+						snippetPaths[i] = string.gsub(str, "/package.json", "")
+					end
+					require("luasnip.loaders.from_vscode").lazy_load({
+						paths = snippetPaths,
+						include = { "typescript", "typescriptreact" },
+					})
 				end,
 			},
 			"saadparwaiz1/cmp_luasnip",
@@ -855,21 +855,15 @@ require("lazy").setup({
 		end,
 	},
 
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	{
 		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+		priority = 1000,
 		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			vim.cmd.colorscheme("tokyonight-night")
 
-			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
+			vim.cmd.hi("VM_Mono guibg=Grey60 guifg=Black gui=NONE")
+			vim.cmd.hi("VM_Cursor guibg=Grey60 guifg=Black gui=NONE")
 		end,
 	},
 	-- Highlight todo, notes, etc in comments
--- .config/nvim/lua/custom/plugins/kulala-http.lua
+++ .config/nvim/lua/custom/plugins/kulala-http.lua
@@ -3,8 +3,26 @@ return {
 	opts = {},
 
 	config = function()
+		-- require("kulala.api").on("after_request", function(data)
+		--
+		-- end)
+
 		vim.keymap.set("n", "<Leader>rr", function()
 			require("kulala").run()
 		end, { desc = "[r]un [r]est command" })
+
+		vim.keymap.set("n", "<Leader>rl", function()
+			require("kulala").inspect()
+		end, { desc = "[r]est: [i]nspect the current request" })
+		vim.keymap.set("n", "]", function()
+			require("kulala").jump_next()
+		end, { desc = "Jump to next rest request" })
+		vim.keymap.set("n", "[", function()
+			require("kulala").jump_prev()
+		end, { desc = "Jump to previous rest request" })
+
+		vim.keymap.set("n", "<Leader>rc", function()
+			require("kulala").copy()
+		end, { desc = "[r]est: [c]opy as curl" })
 	end,
 }
