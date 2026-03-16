81796931d007804d30a8127aa4ef5b6bdbdae75c
Olafur Karl Stefansson <olik@olik.dev>
2026-03-16 15:18:21 +0000

    Attemot to fix TS issues

--- .config/nvim/lua/custom/plugins/lsp_lines.lua
+++ .config/nvim/lua/custom/plugins/lsp_lines.lua
@@ -1,15 +1,15 @@
 return {
 	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-	enabled = false,
+	enabled = true,
 	config = function()
 		require("lsp_lines").setup()
-		local virtual_lines_enabled = true
+		local virtual_lines_enabled = false
 
 		vim.diagnostic.config({
 			virtual_lines = virtual_lines_enabled,
 			virtual_text = not virtual_lines_enabled,
 		})
-		vim.keymap.set("n", "<leader>lt", "Toggle lsp_lines", {
+		vim.keymap.set("n", "<leader>tl", "[t]oggle [l]sp_lines", {
 			callback = function()
 				virtual_lines_enabled = not virtual_lines_enabled
 				vim.diagnostic.config({
